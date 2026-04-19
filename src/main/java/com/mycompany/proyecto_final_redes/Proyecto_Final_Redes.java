7faf2ea92da632819a6614be64b219146718d04f
Luis Alonso <luisalonsoortiz343@gmail.com>
2026-04-18 19:18:32 -0700

    Primer comit del proyecto

---
+++ src/main/java/com/mycompany/proyecto_final_redes/Proyecto_Final_Redes.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyecto_final_redes;
+
+/**
+ *
+ * @author Central
+ */
+public class Proyecto_Final_Redes {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
